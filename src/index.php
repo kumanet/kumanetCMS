b2a0dc66999b09ad3297f4fe27cc6fbd8c5c7b1d
kumanet <webmaster@ku-ma.net>
2017-06-14 16:31:49 +0900

    first commit

---
+++ src/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * ku-ma.net CMS 
+ *
+ * commonのindex
+ * <br>
+ * 各ドメイン共通設定
+ *
+ * @author Nobuyuki Kumakura
+ * @since 2014/09/11
+ * @version 1.2
+ * @date 2014/11/11
+ * 
+ * copyright Nobuyuki Kumakura
+ */
+
+/**
+* 各種phpファイルを読み込む
+*　
+ * @since 2014/11/11
+ * @version 1.2
+*/
+include("setting.php");
+include("langauges.php");
+include("outside.php");
+include("function.php");
+include("variable_setting.php");
+include("include.php");
+include("menubar-navi.php");
+include("contents-navi.php");
+include("info.php");
+//include("counter.php");
+include("sql.php");
+include("responsive-web.php");
+?>
