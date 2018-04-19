3ae8586d754f784a0ba7cdabffc65bb7cfd7b06e
kumanet <webmaster@ku-ma.net>
2018-04-19 17:01:38 +0900

    counter commit

---
+++ src/counter.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ku-ma.net CMS 
+ *
+ * カウンター設定
+ * <br>
+ * クマネットの独自設定あり
+ *
+ * @author Nobuyuki Kumakura
+ * @since 2014/09/19
+ * @version 1.0 
+ * @date 2014/09/19
+ * 
+ * copyright Nobuyuki Kumakura
+ */
+
+/**
+ * 非公開
+ */
+
+
+?>
